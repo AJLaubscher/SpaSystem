8deb3c8a63f37bc641aee0e47260305200a1ba56
BrendanWaspe <brendanwaspe@gmail.com>
2023-09-01 22:06:00 +0200

    Request Reports

---
+++ Spa_Information_System_Group6/Request_Reports.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Spa_Information_System_Group6
+{
+    partial class Request_Reports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.dtStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnDisplayIncome = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnDisplayIncome);
+            this.tabPage1.Controls.Add(this.dtEndDate);
+            this.tabPage1.Controls.Add(this.dtStartDate);
+            this.tabPage1.Controls.Add(this.lstDisplay);
+            this.tabPage1.Controls.Add(this.lblEndDate);
+            this.tabPage1.Controls.Add(this.lblStartDate);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Income Per Time Period";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Location = new System.Drawing.Point(25, 18);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(58, 13);
+            this.lblStartDate.TabIndex = 0;
+            this.lblStartDate.Text = "Start Date:";
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Location = new System.Drawing.Point(387, 18);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(55, 13);
+            this.lblEndDate.TabIndex = 1;
+            this.lblEndDate.Text = "End Date:";
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            this.lstDisplay.Location = new System.Drawing.Point(28, 121);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(716, 264);
+            this.lstDisplay.TabIndex = 2;
+            // 
+            // dtStartDate
+            // 
+            this.dtStartDate.Location = new System.Drawing.Point(100, 18);
+            this.dtStartDate.Name = "dtStartDate";
+            this.dtStartDate.Size = new System.Drawing.Size(200, 20);
+            this.dtStartDate.TabIndex = 3;
+            // 
+            // dtEndDate
+            // 
+            this.dtEndDate.Location = new System.Drawing.Point(461, 18);
+            this.dtEndDate.Name = "dtEndDate";
+            this.dtEndDate.Size = new System.Drawing.Size(200, 20);
+            this.dtEndDate.TabIndex = 4;
+            // 
+            // btnDisplayIncome
+            // 
+            this.btnDisplayIncome.Location = new System.Drawing.Point(28, 74);
+            this.btnDisplayIncome.Name = "btnDisplayIncome";
+            this.btnDisplayIncome.Size = new System.Drawing.Size(716, 41);
+            this.btnDisplayIncome.TabIndex = 5;
+            this.btnDisplayIncome.Text = "Display income per time period";
+            this.btnDisplayIncome.UseVisualStyleBackColor = true;
+            this.btnDisplayIncome.Click += new System.EventHandler(this.btnDisplayIncome_Click);
+            // 
+            // Request_Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Request_Reports";
+            this.Text = "Request_Reports";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnDisplayIncome;
+        private System.Windows.Forms.DateTimePicker dtEndDate;
+        private System.Windows.Forms.DateTimePicker dtStartDate;
+        private System.Windows.Forms.ListBox lstDisplay;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.Label lblStartDate;
+    }
+}
---
+++ Spa_Information_System_Group6/Request_Reports.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spa_Information_System_Group6
+{
+    public partial class Request_Reports : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-NSKM73HE\SQLSERVER;Initial Catalog=SpaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlCommand command;
+        SqlDataAdapter adapt;
+        DataSet ds;
+        SqlDataReader reader;
+        public Request_Reports()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplayIncome_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // open connection
+                conn.Open();
+                string sqlQuery = $"SELECT SUM(Amount_Due) FROM Bookings WHERE Date_Of_Booking BETWEEN '{dtStartDate.Value}' AND '{dtEndDate.Value}'";      // compile select query, Deleted Like '{0}' to only show records that have not been deleted
+                command = new SqlCommand(sqlQuery, conn);
+                adapt = new SqlDataAdapter();
+                ds = new DataSet();
+
+                adapt.SelectCommand = command;                                               // set command for adapter
+                adapt.Fill(ds, "Bookings");                                               // fill dataset
+
+                reader = command.ExecuteReader();
+
+                lstDisplay.Items.Clear(); //Clear listbox
+                while (reader.Read()) //Use while loop
+                {
+                    lstDisplay.Items.Add(reader.GetValue(0) + "\t" + reader.GetValue(1) + "\t" + reader.GetValue(2) + "\t" + reader.GetValue(3) + "\t" + reader.GetValue(4) + "\t" + reader.GetValue(5) + "\t" + reader.GetValue(6) + "\t" + reader.GetValue(7)); //Display database table values in listbox
+                }
+
+                conn.Close();                                                               // close connection
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);                                             // catch error and display message
+            }
+        }
+    }
+}
