2ff74000dce75ed1843619e4555da3b8c9484e89
Chris Kellerman <kellerman.chri@gmail.com>
2023-09-03 13:03:10 +0200

    Commit in order to merge with main

--- Spa_Information_System_Group6/Form1.cs
+++ Spa_Information_System_Group6/Form1.cs
@@ -14,7 +14,7 @@ namespace Spa_Information_System_Group6
     public partial class LogIn : Form
     {
         // Global sql connection
-        SqlConnection conn = new SqlConnection(@"Data Source=MSI\SQLSERVER;Initial Catalog=SpaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-2999I1K;Initial Catalog=SpaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False");
         SqlCommand command;
 
         // variables 
@@ -82,6 +82,11 @@ namespace Spa_Information_System_Group6
             {
                 MessageBox.Show(error.Message);
             }
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
